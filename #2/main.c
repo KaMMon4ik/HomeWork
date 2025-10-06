5844d6a53c4a4b91adff54864a5a56d9247a2617
KaMMon_ <latyshev.arseniy@yandex.ru>
2025-10-06 23:17:53 +0300

    Домашняя работа

--- #2/main.c
+++ #2/main.c
@@ -8,11 +8,11 @@ void main() {
     setlocale(LC_ALL, "RUS");
 
     int x;
-    float v = 4*3;
+    float v = 4 * 3;
 
     printf("Введите расстояние в км, которое пробежал мальчик\n");
     scanf("%d", &x);
 
-    printf("Мальчик пробежал %d км за %.1f ч", x, x/v);
+    printf("Мальчик пробежал %d км за %.1f ч", x, x / v);
 
 }
