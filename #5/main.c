1b070e78940dced0ecc9328b14b40dac43c897cd
KaMMon_ <142399721+KaMMon4ik@users.noreply.github.com>
2025-10-06 23:08:35 +0300

    Домашняя работа

--- #5/main.c
+++ #5/main.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <math.h>
 
-// Вариант 7
+// Г‚Г Г°ГЁГ Г­ГІ 7
 
 void main() {
 
     setlocale(LC_ALL, "RUS");
-
+    
     double x, y, z, gamma;
 
-    printf("Введите x\n");
+    printf("Г‚ГўГҐГ¤ГЁГІГҐ x\n");
     scanf("%lf", &x);
-    printf("Введите y\n");
+    printf("Г‚ГўГҐГ¤ГЁГІГҐ y\n");
     scanf("%lf", &y);
-    printf("Введите z\n");
+    printf("Г‚ГўГҐГ¤ГЁГІГҐ z\n");
     scanf("%lf", &z);
 
     gamma=5*atan(x)-0.25*acos(x)*((x+3*fabs(x-y)+pow(x, 2))/(fabs(x-y)*z+pow(x, 2)));
 
-    printf("Результат = %lf", gamma);
+    printf("ГђГҐГ§ГіГ«ГјГІГ ГІ = %lf", gamma);
 
 }
