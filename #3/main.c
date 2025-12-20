94f87931d0a1fe165dc4a2fd961cba0f0e8ac82b
Арсений <latyshev.arseniy@yandex.ru>
2025-12-20 17:47:18 +0300

    Update

--- #3/main.c
+++ #3/main.c
@@ -7,13 +7,15 @@ void main() {
 
     setlocale(LC_ALL, "RUS");
 
-    int s, t;
+    /*int s, t;
 
     printf("Введите расстояние в км\n");
     scanf("%d", &s);
     printf("Введите стоимость бензина на 1 км\n");
     scanf("%d", &t);
 
-    printf("Стоимость поездки составляет %d руб", s*t);
+    printf("Стоимость поездки составляет %d руб", s*t);*/
+
+    double a = 180.0f * 4 / 5;
 
 }
